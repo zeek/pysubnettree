d0f2bcc4ea86bb77ec5d082659d8cff9eac85240
Robin Sommer <robin@icir.org>
2011-10-25 20:15:08 -0700

    Tweaking setup.py.

--- setup.py
+++ setup.py
@@ -4,15 +4,13 @@ import sys
 
 from distutils.core import setup, Extension
 
-setup(name="pysubnettree", 
-    version="0.14",
-    author="Robin Sommer",
-    author_email="robin@icir.org",
+setup(name="pysubnettree",
+    version="0.14", # Filled in automatically.
+    author_email="info@bro-ids.org",
     license="BSD",
-    url="http://www.icir.org/robin/pysubnettree",
     py_modules=['SubnetTree'],
-    ext_modules = [ 
-        Extension("_SubnetTree", ["SubnetTree.cc", "patricia.c", "SubnetTree_wrap.cc"]), 
-        ] 
+    ext_modules = [
+        Extension("_SubnetTree", ["SubnetTree.cc", "patricia.c", "SubnetTree_wrap.cc"]),
+        ]
 )
 
