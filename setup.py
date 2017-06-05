812f158ab98f3a46d50a0288fbd4d7692f9de651
Johanna Amann <johanna@icir.org>
2017-06-05 10:28:38 -0700

    Updating CHANGES and VERSION.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README') as file:
     long_description = file.read()
 
 setup(name="pysubnettree",
-    version="0.26-2", # Filled in automatically.
+    version="0.27", # Filled in automatically.
     maintainer="The Bro Project",
     maintainer_email="info@bro.org",
     license="BSD",
