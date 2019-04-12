98ffd2093382ff5e794253e694e649254f58f31c
Jon Siwek <jsiwek@corelight.com>
2019-04-12 16:37:51 -0700

    Updating CHANGES and VERSION.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README') as file:
     long_description = file.read()
 
 setup(name="pysubnettree",
-    version="0.29-3", # Filled in automatically.
+    version="0.30", # Filled in automatically.
     maintainer="The Bro Project",
     maintainer_email="info@bro.org",
     license="BSD",
